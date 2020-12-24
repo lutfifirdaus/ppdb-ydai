99ee1f8395c15cbec3686aaf32c065d8491df1ac
Nur Lutfi <mnrltffrds@gmail.com>
2020-12-24 21:34:25 +0700

    fix some view page

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -19,51 +19,6 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset("adminlte/dist/css/adminlte.min.css") }}">
 
-    <style>
-        input::-webkit-outer-spin-button,
-        input::-webkit-inner-spin-button {
-            -webkit-appearance: none;
-            margin: 0;
-        }
-
-        /* Firefox */
-        input[type=number] {
-            -moz-appearance: textfield;
-        }
-        
-        input[type=file] {
-            border: 0px solid black !important;
-        }
-
-        input, select {
-            border: 0px solid black !important;
-            border-radius: 0% !important;
-            border-bottom: 1px solid black !important;
-        }
-
-        .form-control:focus {   
-            border: 0px solid white !important;
-            border-color: none;
-            box-shadow: none;
-            border-bottom: 1px solid blue !important;
-        }
-
-        .card-title {
-            float: none;
-        }
-
-        p {
-            font-size: 14px
-        }
-
-        .nol9{
-            opacity: 0.9
-        }
-
-        .rnd{
-            border-radius: 25px
-        }
-    </style>
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('picture/logo-ydai.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('picture/logo-ydai.png') }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,31 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    @include('layouts.head')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('picture/logo-ydai.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('picture/logo-ydai.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('picture/logo-ydai.png') }}">
+    <link rel="manifest" href="/site.webmanifest">
 
     <style>
         .carousel .carousel-inner img {
@@ -12,7 +36,7 @@
         }
 
         .special {
-            font-size: 92px;
+            font-size: 20px;
             text-align: center;
             text-transform: uppercase;
             text-rendering: optimizeLegibility;
@@ -89,7 +113,7 @@
                     </li>
                     <li class="nav-item">
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 nav-link">Register</a>
+                            <a href="{{ route('register') }}" class="nav-link">Register</a>
                         @endif
                     @endauth
                 </li>
@@ -178,7 +202,7 @@
                         fugiat! Molestias, accusantium! Adipisci ipsum reiciendis voluptate quos vitae inventore
                         doloremque? Minus harum deleniti, rem maxime voluptas laudantium, quas veritatis ullam
                         molestias, placeat excepturi laborum sint dicta!</p>
-                    <div class="justify-content-center">
+                    <div class="d-flex justify-content-between">
                         <button class="btn btn-success btn-xs rnd"><a class="nav-link text-white" href="#">Profil
                                 Sekolah</a></button>
                         <button class="btn btn-success btn-xs rnd"><a class="nav-link text-white" href="#">Visi dan
@@ -201,7 +225,7 @@
                         fugiat! Molestias, accusantium! Adipisci ipsum reiciendis voluptate quos vitae inventore
                         doloremque? Minus harum deleniti, rem maxime voluptas laudantium, quas veritatis ullam
                         molestias, placeat excepturi laborum sint dicta!</p>
-                    <div class="justify-content-center">
+                    <div class="d-flex justify-content-between">
                         <button class="btn btn-danger btn-xs rnd"><a class="nav-link text-white" href="#">Profil
                                 Sekolah</a></button>
                         <button class="btn btn-danger btn-xs rnd"><a class="nav-link text-white" href="#">Visi dan
@@ -234,7 +258,7 @@
                         fugiat! Molestias, accusantium! Adipisci ipsum reiciendis voluptate quos vitae inventore
                         doloremque? Minus harum deleniti, rem maxime voluptas laudantium, quas veritatis ullam
                         molestias, placeat excepturi laborum sint dicta!</p>
-                    <div class="justify-content-center">
+                    <div class="d-flex justify-content-between">
                         <button class="btn btn-primary btn-xs rnd"><a class="nav-link text-white" href="#">Profil
                                 Sekolah</a></button>
                         <button class="btn btn-primary btn-xs rnd"><a class="nav-link text-white" href="#">Visi dan
@@ -257,7 +281,7 @@
                         fugiat! Molestias, accusantium! Adipisci ipsum reiciendis voluptate quos vitae inventore
                         doloremque? Minus harum deleniti, rem maxime voluptas laudantium, quas veritatis ullam
                         molestias, placeat excepturi laborum sint dicta!</p>
-                    <div class="justify-content-center">
+                    <div class="d-flex justify-content-between">
                         <button class="btn btn-secondary btn-xs rnd"><a class="nav-link text-white" href="#">Profil
                                 Sekolah</a></button>
                         <button class="btn btn-secondary btn-xs rnd"><a class="nav-link text-white" href="#">Visi dan
